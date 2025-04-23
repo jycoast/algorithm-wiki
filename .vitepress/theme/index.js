ec05464cc5a1bdb106cce628e215c8df896daa16
jiyongchao <jyc@linux.do>
2025-04-23 18:07:00 +0800

    增加图片缩放功能

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,25 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme-without-fonts'
-// import './my-fonts.css'
+import './global.css'
 
-export default DefaultTheme
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+
+export default {
+    extends: DefaultTheme,
+    setup() {
+      const route = useRoute()
+      const initZoom = () => {
+        // 为所有图片增加缩放功能
+        mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+      }
+      onMounted(() => {
+        initZoom()
+      })
+      watch(
+        () => route.path,
+        () => nextTick(() => initZoom())
+      )
+    }
+  }
